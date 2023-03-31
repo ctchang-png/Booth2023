f8c5be16e385fe7709ba501992f97fe56f9e7e09
ctchang <ctchang001@gmail.com>
2023-03-31 05:42:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def main():
         log_Tx(buffer)
         bytes_written = ser.write(buffer)
         time.sleep(0.050)
-        reply = ser.read(16)
+        reply = ser.read(12)
         log_Rx(reply)
 
         i = (i+1)%n
