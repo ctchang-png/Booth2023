b7a7cf5c1a82cdea177052d4512cb614c50f9761
ctchang <ctchang001@gmail.com>
2023-04-01 22:09:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -32,12 +32,11 @@ def main():
     i = 0
     while True:
         l = lengths[i]
-        l = [1, 2, 3]
+        l = [i, 2, 3]
         cmd = "GOTO" #or "SET " (include space for buffer size)
         buffer = struct.pack(BUFFER_FORMAT, cmd.encode('utf-8'), l[0], l[1], l[2])
         print("Waiting for Pull request")
         signal = ser.read(1)
-        print(signal)
         log_Tx(buffer)
         bytes_written = ser.write(buffer)
         #time.sleep(0.050)
